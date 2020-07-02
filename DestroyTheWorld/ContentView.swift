06a24ea4c1636bdef8a6ef1b9369920dfe9c777a
Ashley Rodriguez <ashleyrodriguez1992@yahoo.com>
2020-07-02 11:49:41 -0400

    hello world

---
+++ DestroyTheWorld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DestroyTheWorld
+//
+//  Created by Ashley Rodriguez on 7/2/20.
+//  Copyright © 2020 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            //Do any additional setup after loading tthe view, typically from a nib.
+        
+        print("Hello World!")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+                //Dispose of any resources that can be recreated.
+    }
+    
+}
